388f445382333491ddd5b3bd999d3e4fb1f5aab2
abdelrhman-saeed <abdelrhmansaeed001@gmail.com>
2025-07-08 19:36:51 +0300

    feat: implemented orders and analytics controllers

---
+++ src/Controllers/OrderController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AnalyticsSystem\Controllers;
+
+use Symfony\Component\HttpFoundation\{Request, Response, JsonResponse};
+use Predis\Client as Redis;
+use AnalyticsSystem\DTO\OrderDTO;
+
+
+class OrderController extends BaseController
+{
+    public function save()
+    {
+        $orderDTO = new OrderDTO(json_decode($this->request->getContent(), true) ?? []);
+
+        if (! empty($orderDTO->getErrors())) {
+            return new JsonResponse($orderDTO->getErrors(), 403);
+        }
+
+        $orderItems = $orderDTO->getValidated() ['products'];
+
+        $this->pdo
+            ->prepare("INSERT INTO orders (created_at) VALUES (datetime('now'))")
+            ->execute();
+
+        $orderId = $this->pdo->lastInsertId();
+
+        $this->pdo->beginTransaction();
+        $stmt = $this->pdo->prepare("INSERT INTO order_items (order_id, product_id, quantity) VALUES (?, ?, ?)");
+
+        foreach($orderItems as $item) {
+            $stmt->execute([$orderId, $item['product_id'], $item['quantity']]);
+        }
+
+        if ($this->pdo->commit()) {
+
+            $redis = new Redis();
+            $redis->publish('broadcast', json_encode((new AnalyticsController)->index()));
+
+            return new JsonResponse(['id' => $orderId] + $orderItems);
+        }
+
+
+        return new Response(status: 500);
+    }
+}
